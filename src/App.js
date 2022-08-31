abcf7fb55714531e8809d6cde743b4b1b9b53769
Акмарал Онгарова <a.ongarova@alagro.local>
2022-08-31 15:42:42 +0600

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,49 +1,76 @@
-import React from 'react';
-import { MapContainer, TileLayer, Polygon, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import { statesData } from './data';
-import './App.css';
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Polygon, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { statesData } from "./data";
+import "./App.css";
 
 const center = [47.654444, 57.9340307];
+// const filteredData = statesData.filter(tsla => tsla.NAME_2 === "xxx")
 
 export default function App() {
+  const [activePark, setActivePark] = useState(0);
   return (
     <MapContainer
       center={center}
       zoom={5}
-      style={{ width: '100vw', height: '100vh' }}
+      style={{ width: "100vw", height: "100vh" }}
     >
+      {activePark && (
+        <Popup
+          position={[
+            activePark.geometry.coordinates[1],
+            activePark.geometry.coordinates[0],
+          ]}
+          onClose={() => {
+            setActivePark(null);
+          }}
+        >
+          <div>
+            <h2>{activePark.properties.NAME_2}</h2>
+          </div>
+        </Popup>
+      )}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
       {statesData.features.map((state) => {
         const coordinates = state.geometry.coordinates[0].map((item) => [
           item[1],
           item[0],
         ]);
-
+        <Marker
+          key={state.properties.NAME_2}
+          position={[
+            state.geometry.coordinates[1],
+            state.geometry.coordinates[0],
+          ]}
+          onClick={() => {
+            setActivePark(state);
+          }}
+        />;
         return (
           <Polygon
             pathOptions={{
-              fillColor: '#FD8D3C',
+              fillColor: "#FD8D3C",
               fillOpacity: 0.7,
               weight: 2,
               opacity: 1,
               dashArray: 3,
-              color: 'white',
+              color: "white",
             }}
             positions={coordinates}
             eventHandlers={{
               mouseover: (e) => {
                 const layer = e.target;
                 layer.setStyle({
-                  dashArray: '',
-                  fillColor: '#BD0026',
+                  dashArray: "",
+                  fillColor: "#BD0026",
                   fillOpacity: 0.7,
                   weight: 2,
                   opacity: 1,
-                  color: 'white',
+                  color: "white",
                 });
               },
               mouseout: (e) => {
@@ -51,16 +78,14 @@ export default function App() {
                 layer.setStyle({
                   fillOpacity: 0.7,
                   weight: 2,
-                  dashArray: '3',
-                  color: 'white',
-                  fillColor: '#FD8D3C',
+                  dashArray: "3",
+                  color: "white",
+                  fillColor: "#FD8D3C",
                 });
               },
               click: (e) => {},
             }}
-          >
-            <Popup>Popup</Popup>
-          </Polygon>
+          ></Polygon>
         );
       })}
     </MapContainer>
