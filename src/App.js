c962a80544ccaca58bf054ad686e190931a52dde
Makoni1 <67749844+Makoni1@users.noreply.github.com>
2022-08-31 23:13:22 +0600

    добавила select

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer, Polygon, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { statesData } from './data';
 import './App.css';
-
+import Select from 'react-select';
 const center = [47.654444, 57.9340307];
 
-
 export default function App() {
-  const [selectValue, setSelectValue] = useState(' ');
+  // const [selectValue, setSelectValue] = useState(' ');
+  const geoObjects = statesData.features.map((state) => {
+    const geoNames = state.properties.NAME_2;
+
+    return geoNames;
+  });
+  // console.log(geoObjects);
+  const newGeoArr = () => {
+    const geoNamesOptions = [];
+    geoObjects.map((item) => {
+      geoNamesOptions.push({ value: item, label: item });
+    });
+
+    return geoNamesOptions;
+  };
+
   return (
     <div className="map-main-container">
-      <input type="text" name="name" className="input__filter" />
-      <select ></select>
+      <Select options={newGeoArr()} className="custom-select" />
       <MapContainer className="map-container" center={center} zoom={5}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+        />
         {statesData.features.map((state) => {
           const coordinates = state.geometry.coordinates[0].map((item) => [
             item[1],
             item[0],
           ]);
-          
-          const geoNames = state.properties.NAME_2;
           <Marker position={center} />;
 
           return (
